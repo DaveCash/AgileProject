5fe6a32bde896be45b8c73d625bdd2e0f13513cb
DaveCash <david-cashman@hotmail.com>
2014-12-11 13:43:44 +0200

    Fixed project edit button

--- KanProject/KanProject.Master.designer.cs
+++ KanProject/KanProject.Master.designer.cs
@@ -82,7 +82,7 @@ namespace KanProject {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hlProjectEdit;
+        protected global::System.Web.UI.WebControls.Button hlProjectEdit;
         
         /// <summary>
         /// hlAnalytics control.
